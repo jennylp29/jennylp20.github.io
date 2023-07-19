3244518af2c649fa5c58dee380dcb88ed9855adb
APRENDIZ <APRENDIZ@BOGCPCDMF11915.FormacionSena.RED>
2023-07-19 12:44:57 -0500

    mensaje de inicio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const suma = () => {
+    let a = 0;
+    let b = 0;
+    let result;
+
+    a = parseInt(prompt("Porfavor ingrese el primer valor"));
+    b = parseInt(prompt("Porfavor ingrese el segundo valor"));
+
+    result = a + b;
+    alert("La suma es igual a: " + result);
+}
